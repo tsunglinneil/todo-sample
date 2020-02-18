3188e33f9b68fa1b8cdec3009ec2daeb412f7557
tsunglinneil <magic3657@gmail.com>
2020-02-18 15:17:02 +0800

    init sample

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>Todo App</h1>
+    
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+@import "./variables.scss";
+
+#app {
+  max-width: 400px;
+  margin: 0 auto;
+  line-height: 1.4;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: $vue-blue;
+}
+
+h1 {
+  text-align: center;
+}
+</style>
---
+++ src/components/TaskInput.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+@import "../../variables.scss";
+
+.input {
+  width: 100%;
+  padding: 8px 10px;
+  border: 1px solid $vue-blue;
+}
+</style>
